9cf8f758945cd7a2bac86a8199fb28b80a8f77b2
Bajracharya <bajracharyaa@etsu.edu>
2024-08-26 14:40:29 -0400

    feat: band name generator

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello! Welcome to my Band Name Creator Application")
+city = input("What city did you grew up? \n")
+pet = input("What is the name of your pet? \n")
+print("Your band name is:" + city + pet)
