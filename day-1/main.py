02c777c6de1f6e429531d98b8b1d4987c195f34c
Bajracharya <bajracharyaa@etsu.edu>
2024-08-29 11:23:22 -0400

    Day 2 BMI calculator challenge

--- day-1/main.py
+++ day-1/main.py
@@ -2,3 +2,5 @@ print("Hello! Welcome to my Band Name Creator Application")
 city = input("What city did you grew up? \n")
 pet = input("What is the name of your pet? \n")
 print("Your band name is:" + city + pet)
+
+print("Type is:", type(city))
---
+++ day-2/bmiCalc.py
@@ -0,0 +1,6 @@
+length = float(input("Enter height in metres"))
+weight = int(input("Enter weight in kgs"))
+
+bmi = weight/length**2
+
+print("Your body mass index is:", bmi)
