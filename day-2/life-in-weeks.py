b29518f9438812c2d3bec40c7d27c72ecdb2703c
Bajracharya <bajracharyaa@etsu.edu>
2024-08-29 11:40:37 -0400

    Day 2 Challenge: Calculate life in weeks

---
+++ day-2/life-in-weeks.py
@@ -0,0 +1,8 @@
+age = input("Enter your age:")
+
+remYears = 90 - int(age)
+remDays = remYears * 365
+remMonths = remYears * 12
+remWeeks = remYears * 52
+
+print(f"You have {remWeeks} weeks, {remDays} days and {remMonths} months to live if you live till 90 years old")
